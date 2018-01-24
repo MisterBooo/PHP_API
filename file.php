df2773fb214c3bccf3c834d9b25b8b210a8c4941
MisterBigbooo <278166530@qq.com>
2018-01-24 19:54:54 +0800

    读取缓存方式开发首页接口

--- file.php
+++ file.php
@@ -12,9 +12,10 @@ class File{
     private $_dir;
     const EXT = '.txt';
     /**
-     *
+     * 构造函数
+     * 非常适合在使用对象之前做一些初始化工作
      */
-    public function _construct(){
+    public function __construct(){
         $this->_dir = dirname(__FILE__) . '/files/';
     }
     /**
@@ -22,7 +23,7 @@ class File{
      * @param string $value
      * @param string $path
      */
-    public function cacheData($key, $value='', $path=''){
+    public function cacheData($key, $value='', $cacheTime = 0){
 
         $filename = $this->_dir  . $key . self::EXT;
 
@@ -35,19 +36,29 @@ class File{
             if(!is_dir($dir)) {
                 mkdir($dir, 0777);
             }
-           return file_put_contents($filename,json_encode($value));
+            $cacheTime = sprintf('%011d',$cacheTime);
+           return file_put_contents($filename,$cacheTime.json_encode($value));
         }
 
         if (!is_file($filename)){
             return FALSE;
-        }else{
-            return json_decode(file_get_contents($filename),true);
+        }
+        $contents = file_get_contents($filename);
+        $cacheTime = (int)substr($contents,0,11);
+        $value = substr($contents, 11);
+        if ($cacheTime != 0 && ($cacheTime + filemtime($filename) < time())){
+            //缓存时间到期了，删除缓存
+            unlink($filename);
+            return FALSE;
         }
 
+        return json_decode($value,true);
 
 
     }
 
 }
 
+
 ?>
+
--- list.php
+++ list.php
@@ -7,6 +7,12 @@
  */
 require_once ('./response.php');
 require_once ('./db.php');
+require_once ('./file.php');
+
+
+
+//先去查询缓存
+$file = new File();
 
 $page = isset($_GET['page'])? $_GET['page']:1;
 $pageSize = isset($_GET['pageSize'])?$_GET['pageSize']:6;
@@ -16,20 +22,32 @@ if (!is_numeric($page)||!is_numeric($pageSize)) {
 $offset = ($page - 1) * $pageSize;
 $sql = "select * from cccc WHERE c = 2 limit ".$offset.",".$pageSize ;
 
-try{
-    $connect = Db::getInstance()->connect();
-}catch (Exception $e){
-    return Response::show('403','数据库连接失败');
-}
 
-
-
-$result = mysqli_query($connect,$sql);
-
+$cache = new File();
 $videos = array();
-while ($video = mysqli_fetch_assoc($result)){
-    $videos[] = $video;
+if (!$cache ->cacheData('index_mk_cache'.$page.'-'.$pageSize)){ //没缓存或者缓存过期了
+    try{
+        $connect = Db::getInstance()->connect();
+    }catch (Exception $e){
+        return Response::show('403','数据库连接失败');
+    }
+    $result = mysqli_query($connect,$sql);
+
+    while ($video = mysqli_fetch_assoc($result)){
+        $videos[] = $video;
+    }
+    if ($videos){
+        $cache ->cacheData('index_mk_cache'.$page.'-'.$pageSize,$videos,120);
+    }
+
+}else{//直接读取缓存
+    $videos = $cache ->cacheData('index_mk_cache'.$page.'-'.$pageSize);
 }
+
+
+
+
+
 if ($videos){
     return Response::show('200','首页数据获取成功',$videos);
 }else{
