10d8190c9d8318bf607044269c8b99b468269d97
MisterBigbooo <278166530@qq.com>
2018-01-24 13:58:34 +0800

    mysqli操作

--- db.php
+++ db.php
@@ -8,6 +8,14 @@
 
 class Db{
     static private $_instance;
+    static private $_connectSource;
+    private $_dbConfig = array(
+        'host'=>'localhost',
+        'user'=>'root',
+        'password'=>'123123',
+        'database'=>'testDatabase'
+    );
+
 
     private function _construct(){
 
@@ -23,7 +31,27 @@ class Db{
         return self::$_instance;
     }
 
+    public function connect(){
+        if (!self::$_connectSource) {
+            //连接数据库
+            self::$_connectSource = mysqli_connect($this->_dbConfig['host'], $this->_dbConfig['user'], $this->_dbConfig['password']);
+            if (!self::$_connectSource){
+                die('mysql connect error '.mysqli_error());
+            }
+            //选择数据库
+            mysqli_select_db(self::$_connectSource,$this->_dbConfig['database']);
+        }
+       return self::$_connectSource;
+    }
+
 }
+$connect = Db::getInstance()->connect();
+$sql = 'select * from cccc';
+//查询数据库
+$result = mysqli_query($connect,$sql);
+echo mysqli_num_rows($result);
+
+
 
 
 
