78c9fae23fd20c89080aa663d9287c849cc777c3
MisterBigbooo <278166530@qq.com>
2018-01-24 09:32:40 +0800

    配置host与Apache

---
+++ list.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by MisterBigbooo.
+ * User: Zeno
+ * Date: 2018/1/23
+ * Time: 下午7:48
+ */
+require_once ('./response.php');
+$page = isset($_GET['page'])? $_GET['page']:1;
+$pageSize = isset($_GET['pagesize'])?$_GET['pagesize']:1;
+
+
+?>
